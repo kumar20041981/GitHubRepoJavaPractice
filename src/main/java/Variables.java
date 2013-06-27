648ea8c26dbe9c3221cb663514ee8c1c783f7587
kumar20041981 <sunil_vooturi@yahoo.com>
2013-06-27 22:30:29 +0100

    second java file

---
+++ src/main/java/Variables.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sunil
+ * Date: 6/27/13
+ * Time: 10:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Variables {
+    public static void main(String[] args) {
+        System.out.println("this is variables pattern");
+    }
+}
