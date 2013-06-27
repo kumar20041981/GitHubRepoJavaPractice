efd77be182412e6999e5159c0655c8566c971c84
kumar20041981 <sunil_vooturi@yahoo.com>
2013-06-27 22:27:54 +0100

    First java file

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sunil
+ * Date: 6/27/13
+ * Time: 10:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Welcome to java");
+    }
+}
