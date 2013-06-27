99d1534709b8a3be33374b6c5dc02afa0f4cc934
kumar20041981 <sunil_vooturi@yahoo.com>
2013-06-27 22:44:41 +0100

    if else condition file

---
+++ src/main/java/IfElseCondition.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sunil
+ * Date: 6/27/13
+ * Time: 10:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class IfElseCondition {
+    public static void main(String[] args) {
+        boolean b= true;
+        if(b){
+        System.out.println("boolean b is true");
+         }  else if(!b){
+            System.out.println("boolean b is false");
+        }
+    }
+}
